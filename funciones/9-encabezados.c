8b95787c39c4b9696eab3c1e2529b079b1622b56
cesar-21 <cesar.alvaradocastro@ucr.ac.cr>
2024-05-02 23:50:46 -0600

    Cambios en feature-branch

--- funciones/9-encabezados.c
+++ funciones/9-encabezados.c
@@ -13,6 +13,11 @@ int main() {
     // Imprime el valor que retornó funcion3()
     printf("v = %d\n", v);
 
+    v = funcion5(10);
+
+    // Imprime el valor que retornó funcion5()
+    printf("v = %d\n", v);
+
     return 0;
 
 }
@@ -35,3 +40,12 @@ int funcion3(int x) {
     int y = x + 2;
     return y;
 }
+
+// funcion5(): Imprime un mensaje. Retorna el valor que recibió como argumento
+// incrementado en 3
+int funcion5(int x) {
+    printf("En funcion5()...\n");
+    int y = x + 3;
+    return y;
+}
+
